01d3beee58df59fb8e50cb59f88450e5327e0baf
Shehab Abdel-Salam <shehab_m@aucegypt.edu>
2016-12-06 13:51:03 +0200

    Trying color spaces to detect hand

--- Project_Vision/main.cpp
+++ Project_Vision/main.cpp
@@ -16,8 +16,8 @@
 using namespace std;
 using namespace cv;
 
-void RGB_Range(Mat, Mat&);
-void HSV_Range(Mat, Mat&);
+void RGB_Range(Mat&);
+void HSV_Range(Mat&);
 
 int main()
 {
@@ -29,32 +29,45 @@ int main()
     cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
     cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
     
+    
     while(true)
     {
-        Mat frame, frame_HSV;
-        Mat RGB, HSV;
+        Mat frame, frame_HSV, frame_YCRCB;
+        Mat Skin_Mask1, Skin_Mask2;
+        Mat Contours, Hierarchy;
         cap.read(frame);
+        auto lower_HSV = Scalar(0, 48, 80);
+        auto upper_HSV = Scalar(20, 255, 255);
         
-        RGB.create(frame.rows, frame.cols, CV_LOAD_IMAGE_GRAYSCALE);
-        HSV.create(frame.rows, frame.cols, CV_LOAD_IMAGE_GRAYSCALE);
-
+        auto lower_YCRCB = Scalar(0, 133, 77);
+        auto upper_YCRCB = Scalar(255, 173, 127);
         cvtColor(frame, frame_HSV, CV_BGR2HSV);
-        RGB_Range(frame, RGB);
-        HSV_Range(frame_HSV, HSV);
-        //blur(frame, frame, Size(5,5));
+        cvtColor(frame, frame_YCRCB, CV_BGR2YCrCb);
+
+        inRange(frame_HSV, lower_HSV, upper_HSV, Skin_Mask1);
+        inRange(frame_YCRCB, lower_YCRCB, upper_YCRCB, Skin_Mask2);
+        
+        
+        //RGB.create(frame.rows, frame.cols, CV_LOAD_IMAGE_GRAYSCALE);
+        //HSV.create(frame.rows, frame.cols, CV_LOAD_IMAGE_GRAYSCALE);
+
+        //RGB_Range(frame);
+        //HSV_Range(frame_HSV, HSV);
+        //blur(Skin_Mask2, Skin_Mask2, Size(3,3));
         //Canny(frame, Contours, 40, 120);
         
         imshow("Frame", frame);
         imshow("HSV", frame_HSV);
-        //imshow("RGB", RGB);
-        //imshow("HSV", HSV);
+        imshow("YCRCB", frame_YCRCB);
+        imshow("Skin Mask (HSV)", Skin_Mask1);
+        imshow("Skin Mask (YCRCB)", Skin_Mask2);
     }
     //////////////////////////////////////////
     
     return 0;
 }
 
-void RGB_Range(Mat frame, Mat& new_frame)
+void RGB_Range(Mat& frame)
 {
     for(int i=0; i<frame.rows; i++)
     {
@@ -66,14 +79,16 @@ void RGB_Range(Mat frame, Mat& new_frame)
             //TODO
             if(Red >= 100 && Green >= 100 && Blue >= 100)
             {
-                new_frame.at<uchar>(i, j) = 255;
+                frame.at<Vec3b>(i, j)[2] = 255;
+                frame.at<Vec3b>(i, j)[1] = 255;
+                frame.at<Vec3b>(i, j)[0] = 255;
             }
                 
         }
     }
 }
 
-void HSV_Range(Mat frame, Mat& new_frame)
+void HSV_Range(Mat& frame)
 {
     for(int i=0; i<frame.rows; i++)
     {
@@ -84,7 +99,11 @@ void HSV_Range(Mat frame, Mat& new_frame)
             int V = frame.at<Vec3b>(i, j)[2];
             //TODO
             if(H >= 100 && S >= 100 && V >= 100)
-                new_frame.at<uchar>(i, j) = 255;
+            {
+                frame.at<Vec3b>(i, j)[0] = 255;
+                frame.at<Vec3b>(i, j)[1] = 255;
+                frame.at<Vec3b>(i, j)[2] = 255;
+            }
         }
     }
 }
